aba19b3a8227c003674aa460029eff550af06612
MarlonSystems <marlon.25.perka@gmail.com>
2026-01-22 22:11:52 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from database import init_db
+from routes import api
+
+app = Flask(__name__)
+app.register_blueprint(api)
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
