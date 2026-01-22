c8c47d695d2cffd3b18355e9bdcf383e1cf87910
MarlonSystems <marlon.25.perka@gmail.com>
2026-01-22 22:26:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from database import init_db
 from routes import api
 
 app = Flask(__name__)
 app.register_blueprint(api)
+
+@app.route("/")
+def dashboard():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     init_db()
